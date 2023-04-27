9774e415c7c7b6867c2692e6b73fd404f229e4f1
hari <agraharamroyal@gmail.com>
2023-04-27 15:47:27 +0530

    first commit

---
+++ Empclient copy/src/main/java/com/pro/emp/EmpclientApplication.java
@@ -0,0 +1,13 @@
+package com.pro.emp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EmpclientApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EmpclientApplication.class, args);
+	}
+
+}
---
+++ Empclient copy/src/test/java/com/pro/emp/EmpclientApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pro.emp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EmpclientApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
